b3f87a5efbfd91419ac255f8a7365c7494891295
Pablo <pabloerhard02@gmail.com>
2023-06-04 20:18:21 -0600

    Svelte basic styling button

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+import Button from './lib/Button.svelte'
+import GoPrimitiveDot from 'svelte-icons/go/GoPrimitiveDot.svelte'
+import DiGithubFull from 'svelte-icons/di/DiGithubFull.svelte'
+</script>
+
+<Button on:click={()=>alert(true)} let:isLeftHovered size={'large'} shadow={true} bgColor="red" textColor="white" >
+    <div style:width="20px" slot="leftContent">
+        {#if isLeftHovered}
+        <DiGithubFull/>
+        {:else}
+        <GoPrimitiveDot />
+        {/if}
+    </div>
+    Button 
+</Button>
+
+<style>
+
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,19 @@
+<script>
+  let count = 0;
+  const increment = () =>{
+      count+=1
+  }
+</script>
+
+<h1>{count}</h1>
+<button on:click={increment} >
+Increment
+</button>
+
+<style>
+  button {
+    padding:15px 20px;
+
+  }
+</style>
+
